fa605e3bb519bf4725f73c7acd608a5fff3f704c
Tyler <tylerpoore2000@gmail.com>
2019-07-25 20:19:46 -0700

    add test router

--- src/routes/static.js
+++ src/routes/static.js
@@ -5,4 +5,8 @@ router.get("/", (req, res, next) => {
    res.send("Welcome to Bloccit! ");
 });
 
+router.get("/test", (req, res, next) => {
+   res.send("<a href='https://google.com' title='Google'>Test</a>")
+});
+
 module.exports = router;
