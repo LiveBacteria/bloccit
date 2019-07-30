71b0ece21aa374ba63a9fc7cefeb3ce600489a56
Tyler <tylerpoore2000@gmail.com>
2019-07-29 20:10:23 -0700

    Stuck on running npm jasmine test

---
+++ src/db/models/topic.js
@@ -0,0 +1,21 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Topic = sequelize.define('Topic', {
+    title: DataTypes.STRING,
+    description: DataTypes.STRING
+  }, {});
+  Topic.associate = function(models){
+    Topic.hasMany(models.Rule, {
+      foreignKey: "topicId",
+      as: "rules"
+    });
+  }
+  Topic.associate = function(models) {
+    // associations can be defined here
+    Topic.hasMany(models.Banner, {
+      foreignKey: "topicId",
+      as: "banners",
+    });
+  };
+  return Topic;
+};
