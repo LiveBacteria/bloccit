2c8e389b95c3229e24d6fa6b37d97b63d6c1416f
Tyler <tylerpoore2000@gmail.com>
2019-09-18 21:29:03 -0700

    Issues with Authenticator

---
+++ src/db/queries.users.js
@@ -0,0 +1,16 @@
+const User = require("./models").User;
+const bcrypt = require("bcryptjs");
+
+module.exports = {
+    createUser(newUser, callback) {
+        const salt = bcrypt.getSaltSync();
+        const hashedPassword = bcrypt.hashSync(newUser.password, salt);
+
+        return User.create({
+            email: newUser.email,
+            password: hashedPassword
+        }).then((user) => {
+            callback(null, user);
+        }).catch(err => callback(err));
+    }
+}
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const validation = require("./validations");
 
 const userController = require("../controllers/userController");
 
-router.get("/users/sign_up", userController.signUp);
+router.get("/users/sign_up", userController.sign_up);
+router.post("/users", validation.validateUsers, userController.create);
 
 module.exports = router;
