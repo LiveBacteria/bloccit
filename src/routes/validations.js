a3b5ad9cc6c6f27212168a2048e80685c1ff0c81
Tyler <tylerpoore2000@gmail.com>
2019-09-19 17:26:39 -0700

    Authentication issue 2

--- src/routes/validations.js
+++ src/routes/validations.js
@@ -1,5 +1,5 @@
 module.exports = {
-    validatePosts(req, res, next) {f
+    validatePosts(req, res, next) {
         if(req.method === "POST") {
             req.checkParams("topicId", "must be valid").notEmpty().isInt();
             req.checkBody("title", "must be at least 2 characters in length").isLength({min: 2});
